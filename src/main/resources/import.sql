4cf4a1ba1b964c5ad529d087b5a237ac4980fa47
kendrang <kendrang@users.noreply.github.com>
2019-12-09 17:15:36 -0500

    fixed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,76 +6,76 @@ insert into option (option_id, option_value, poll_id) values (4, 'Blue', 1);
 insert into option (option_id, option_value, poll_id) values (5, 'Green', 1);
 
 insert into poll (poll_id, question) values (2, 'What is your favorite Pokemon Starter?');
-insert into option (option_id, option_value, poll_id) values (1, 'Charmander', 2);
-insert into option (option_id, option_value, poll_id) values (2, 'Bulbasaur', 2);
-insert into option (option_id, option_value, poll_id) values (3, 'Squirtle', 2);
-insert into option (option_id, option_value, poll_id) values (4, 'Pikachu', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'Charmander', 2);
+insert into option (option_id, option_value, poll_id) values (7, 'Bulbasaur', 2);
+insert into option (option_id, option_value, poll_id) values (8, 'Squirtle', 2);
+insert into option (option_id, option_value, poll_id) values (9, 'Pikachu', 2);
 
 insert into poll (poll_id, question) values (3, 'Who is the best Teen Titan? ');
-insert into option (option_id, option_value, poll_id) values (1, 'Robin', 3);
-insert into option (option_id, option_value, poll_id) values (2, 'Raven', 3);
-insert into option (option_id, option_value, poll_id) values (3, 'Beast boy', 3);
-insert into option (option_id, option_value, poll_id) values (4, 'Starfire', 3);
-insert into option (option_id, option_value, poll_id) values (5, 'Cyborg', 3);
+insert into option (option_id, option_value, poll_id) values (10, 'Robin', 3);
+insert into option (option_id, option_value, poll_id) values (11, 'Raven', 3);
+insert into option (option_id, option_value, poll_id) values (12, 'Beast boy', 3);
+insert into option (option_id, option_value, poll_id) values (13, 'Starfire', 3);
+insert into option (option_id, option_value, poll_id) values (14, 'Cyborg', 3);
 
 insert into poll (poll_id, question) values (4, 'What is your favorite Dessert?');
-insert into option (option_id, option_value, poll_id) values (1, 'Cake', 4);
-insert into option (option_id, option_value, poll_id) values (2, 'Cookie', 4);
-insert into option (option_id, option_value, poll_id) values (3, 'Ice Cream', 4);
+insert into option (option_id, option_value, poll_id) values (15, 'Cake', 4);
+insert into option (option_id, option_value, poll_id) values (16, 'Cookie', 4);
+insert into option (option_id, option_value, poll_id) values (17, 'Ice Cream', 4);
 
 insert into poll (poll_id, question) values (5, 'What is your favorite Number?');
-insert into option (option_id, option_value, poll_id) values (1, 'One', 5);
-insert into option (option_id, option_value, poll_id) values (2, 'Two', 5);
-insert into option (option_id, option_value, poll_id) values (3, 'Three', 5);
+insert into option (option_id, option_value, poll_id) values (18, 'One', 5);
+insert into option (option_id, option_value, poll_id) values (19, 'Two', 5);
+insert into option (option_id, option_value, poll_id) values (20, 'Three', 5);
 
 
 insert into poll (poll_id, question) values (6, 'What is your favorite Ghibli Movie?');
-insert into option (option_id, option_value, poll_id) values (1, 'Spirited Away', 6);
-insert into option (option_id, option_value, poll_id) values (2, 'Totoro', 6);
-insert into option (option_id, option_value, poll_id) values (3, 'Howls Moving Castle', 6);
+insert into option (option_id, option_value, poll_id) values (21, 'Spirited Away', 6);
+insert into option (option_id, option_value, poll_id) values (22, 'Totoro', 6);
+insert into option (option_id, option_value, poll_id) values (23, 'Howls Moving Castle', 6);
 
 insert into poll (poll_id, question) values (7, 'What is your favorite Ghibli Movie?');
-insert into option (option_id, option_value, poll_id) values (1, 'Princess Mononoke', 7);
-insert into option (option_id, option_value, poll_id) values (2, 'Castle in the sky', 7);
-insert into option (option_id, option_value, poll_id) values (3, 'Valley of the Wind', 7);
+insert into option (option_id, option_value, poll_id) values (24, 'Princess Mononoke', 7);
+insert into option (option_id, option_value, poll_id) values (25, 'Castle in the sky', 7);
+insert into option (option_id, option_value, poll_id) values (26, 'Valley of the Wind', 7);
 
 insert into poll (poll_id, question) values (8, 'What is your favorite pasta?');
-insert into option (option_id, option_value, poll_id) values (1, 'Spaghetti', 8);
-insert into option (option_id, option_value, poll_id) values (2, 'Rigatoni', 8);
-insert into option (option_id, option_value, poll_id) values (3, 'Penne', 8);
+insert into option (option_id, option_value, poll_id) values (27, 'Spaghetti', 8);
+insert into option (option_id, option_value, poll_id) values (28, 'Rigatoni', 8);
+insert into option (option_id, option_value, poll_id) values (29, 'Penne', 8);
 
 insert into poll (poll_id, question) values (9, 'What is your favorite Shrek Movie? ');
-insert into option (option_id, option_value, poll_id) values (1, 'Shrek 1', 9);
-insert into option (option_id, option_value, poll_id) values (2, 'Shrek 2', 9);
-insert into option (option_id, option_value, poll_id) values (3, 'Shrek 3', 9);
+insert into option (option_id, option_value, poll_id) values (30, 'Shrek 1', 9);
+insert into option (option_id, option_value, poll_id) values (31, 'Shrek 2', 9);
+insert into option (option_id, option_value, poll_id) values (32, 'Shrek 3', 9);
 
 insert into poll (poll_id, question) values (10, 'What is the worst Movie?');
-insert into option (option_id, option_value, poll_id) values (1, 'Bee Movie', 10);
-insert into option (option_id, option_value, poll_id) values (2, 'Angry Birds', 10);
-insert into option (option_id, option_value, poll_id) values (3, 'Emoji Movie', 10);
+insert into option (option_id, option_value, poll_id) values (33, 'Bee Movie', 10);
+insert into option (option_id, option_value, poll_id) values (34, 'Angry Birds', 10);
+insert into option (option_id, option_value, poll_id) values (35, 'Emoji Movie', 10);
 
 insert into poll (poll_id, question) values (11, 'Whose your favorite instructor?');
-insert into option (option_id, option_value, poll_id) values (1, 'Roberto', 11);
-insert into option (option_id, option_value, poll_id) values (2, 'Dolio', 11);
-insert into option (option_id, option_value, poll_id) values (3, 'Froilan', 11);
-insert into option (option_id, option_value, poll_id) values (4, 'Chris', 11);
+insert into option (option_id, option_value, poll_id) values (36, 'Roberto', 11);
+insert into option (option_id, option_value, poll_id) values (37, 'Dolio', 11);
+insert into option (option_id, option_value, poll_id) values (38, 'Froilan', 11);
+insert into option (option_id, option_value, poll_id) values (39, 'Chris', 11);
 
 insert into poll (poll_id, question) values (12, 'What drink do you like in the morning?');
-insert into option (option_id, option_value, poll_id) values (1, 'Coffee', 12);
-insert into option (option_id, option_value, poll_id) values (2, 'Tea', 12);
-insert into option (option_id, option_value, poll_id) values (3, 'Orange Juice', 12);
+insert into option (option_id, option_value, poll_id) values (40, 'Coffee', 12);
+insert into option (option_id, option_value, poll_id) values (41, 'Tea', 12);
+insert into option (option_id, option_value, poll_id) values (42, 'Orange Juice', 12);
 
 insert into poll (poll_id, question) values (13, 'Whats your favorite milk flavor?');
-insert into option (option_id, option_value, poll_id) values (1, 'Chocolate', 13);
-insert into option (option_id, option_value, poll_id) values (2, 'Strawberry', 13);
-insert into option (option_id, option_value, poll_id) values (3, 'Plain', 13);
+insert into option (option_id, option_value, poll_id) values (43, 'Chocolate', 13);
+insert into option (option_id, option_value, poll_id) values (44, 'Strawberry', 13);
+insert into option (option_id, option_value, poll_id) values (45, 'Plain', 13);
 
 insert into poll (poll_id, question) values (14, 'Whats your favorite Phone?');
-insert into option (option_id, option_value, poll_id) values (1, 'Apple', 14);
-insert into option (option_id, option_value, poll_id) values (2, 'Android', 14);
-insert into option (option_id, option_value, poll_id) values (3, 'Other', 14);
+insert into option (option_id, option_value, poll_id) values (46, 'Apple', 14);
+insert into option (option_id, option_value, poll_id) values (47, 'Android', 14);
+insert into option (option_id, option_value, poll_id) values (48, 'Other', 14);
 
 insert into poll (poll_id, question) values (15, 'Whats your favorite Fast Food place?');
-insert into option (option_id, option_value, poll_id) values (1, 'McDonalds', 15);
-insert into option (option_id, option_value, poll_id) values (2, 'Burger King', 15);
-insert into option (option_id, option_value, poll_id) values (3, 'Wendys', 15);
+insert into option (option_id, option_value, poll_id) values (49, 'McDonalds', 15);
+insert into option (option_id, option_value, poll_id) values (50, 'Burger King', 15);
+insert into option (option_id, option_value, poll_id) values (51, 'Wendys', 15);
